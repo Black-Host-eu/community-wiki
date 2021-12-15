760217c487031fee6b65e87f9adab8c5ab90f1f9
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 22:04:31 +0100

    [!] Fehler im Link ehoben

--- wie-erstelle-ich-eine-website.php
+++ wie-erstelle-ich-eine-website.php
@@ -16,7 +16,7 @@
   1. Logge dich auf <a href="https://vweb01.host-control.eu">hier klicken</a>	mit den aus dem Panel erhaltenen Zugangsdaten ein.
   2. Danach müsstest du dieses Fenster sehen: <img src="https://prnt.sc/235uipk" alt="ScreenPlesk">
   3. Hier klickst du auf "Domain hinzufügen": <img src="https://prnt.sc/235umsx" alt="ScreenDomain">
-  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du hier <a href="HIER LINK ZU DNS VERWALTUNG"></a> <img src="https://prnt.sc/235utsd" alt="">
+  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du <a href="dns-einträge.php">hier </a>. Nachdem du das gemacht hast, kannst du die Daten hier eintragen <img src="https://prnt.sc/235utsd" alt="">
   4.1 Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst: <img src="https://prnt.sc/235v4av" alt="">
   5. Nach dem eingeben, klickst du auf Domain hinzufügen <img src="https://prnt.sc/235v9sd" alt="">
 
