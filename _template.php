aed11d00317241a60f26719d4934a92fc609497c
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-12-15 15:59:04 +0100

    Create _template.php

---
+++ _template.php
@@ -0,0 +1,41 @@
+										
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">								
+  <a href="#deine-überschrift" data-kt-scroll-toggle=""></a>				
+  Deine Überschrift
+</h1>
+<!-- Überschrift -->	
+
+<!-- -->							
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+	
+  Ein kleiner Text 1
+						
+  <br><br>
+  
+  Ein kleiner Text 2		
+</div>
+									
+</div>
+<!-- Text zur Überschrift -->
+
+<!-- -->
+
+<!-- Normaler Absatz -->							
+<div class="pb-10">
+										
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#eine-überschrift" data-kt-scroll-toggle=""></a>
+    Eine Überschrift
+  </h1>
+										
+  <div class="py-5">
+									
+    Dein Text
+							
+  </div>
+									
+</div>
+<!-- Normaler Absatz -->
