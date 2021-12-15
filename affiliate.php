893b5732ec0fff01ae8a72bec86f354b55869880
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 21:35:50 +0100

    [+] Affiliate System

---
+++ affiliate.php
@@ -0,0 +1,59 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#affiliate" data-kt-scroll-toggle=""></a>
+  Affiliate-System
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Seit der neusten Panel Version haben wir ein Affiliate-System, wodurch du Geld verdienen kannst, indem Leute deinen Link anklicken (genial oder?).
+
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->
+<div class="pb-10">
+
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#auszahlung" data-kt-scroll-toggle=""></a>
+   Wie funktioniert die Auszahlung
+  </h1>
+
+  <div class="py-5">
+
+    Eine Auszahlung kannst du im Support anfragen. Dies wird benötigt um "Betrüger", die Aufrufe über andere Server generieren,
+    nicht die Möglichkeit zu geben das Geld auszahlen zu lassen. Vor der Auszahlung erfolgt eine automatisierte Prüfung deiner Aufrufe.
+
+
+  </div>
+
+</div>
+<!-- Normaler Absatz -->
+<div class="pb-10">
+
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#ausnutzung" data-kt-scroll-toggle=""></a>
+    Was passiert, wenn das Affiliate System ausgenutzt wird?
+  </h1>
+
+  <div class="py-5">
+
+    Unter Ausnutzen dieses Systems verstehen wir einen Betrug über IPs durchzuführen.
+     Wie genau das geht werden wir, logischer Weise, nicht erklären.
+     Die Folgen eines solchen Betruges reichen von einer Sperre des Affiliate System
+     bis hin zu einer Sperrung des Kundenkontos.
+    Weitere Fragen zu diesem Thema sind im <a href="https://cp.black-host.eu/tickets">Support</a> erwünscht.
+
+  </div>
+
+</div>
