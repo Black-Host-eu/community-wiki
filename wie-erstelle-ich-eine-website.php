0e98c4299b1b5eddddca69c79bbf9dc993fef046
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 21:57:24 +0100

    [+] Neuer Beitrag

--- wie-erstelle-ich-eine-website.php
+++ wie-erstelle-ich-eine-website.php
@@ -29,19 +29,4 @@
 
 <!-- -->
 
-<!-- Normaler Absatz -->
-<div class="pb-10">
 
-  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
-    <a href="#eine-überschrift" data-kt-scroll-toggle=""></a>
-    Eine Überschrift
-  </h1>
-
-  <div class="py-5">
-
-    Dein Text
-
-  </div>
-
-</div>
-<!-- Normaler Absatz -->
