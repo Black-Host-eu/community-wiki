344c750978911ed0a9a4c800f097d48753b50796
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 22:05:08 +0100

    [+] DNS Einträge für VWEB01

---
+++ dns-einträge.php
@@ -0,0 +1,28 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#dns" data-kt-scroll-toggle=""></a>
+  Welche DNS Einträge brauche ich für den WebServer?
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Für unseren VWEB01 musst du für deine Domain folgende Einträge setzen.
+ 	Da ich deine Domain nicht kenne, habe ich den Platzhalter deinedomain.de gewählt. Dies ist aber analog bei allen anderen Domains.
+  Also - du musst überall deinedomain.de durch deine Domain (bspw. black-host.eu, google.de)
+
+  @         --> A     --> 88.198.11.120
+  webmail   --> A     --> 88.198.11.120
+  mail      --> A     --> 88.198.11.120
+  @         --> MX(10)--> mail.deinedomain.de
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
