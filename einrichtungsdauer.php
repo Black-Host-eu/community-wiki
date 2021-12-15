3413448aff38455e47f24e064c74ddd7bba7f926
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 21:08:47 +0100

    [+] Einrichtungsdauer der Produkte
    neuer Wiki-Eintrag

---
+++ einrichtungsdauer.php
@@ -0,0 +1,25 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#einrichtungsdauer" data-kt-scroll-toggle=""></a>
+  Einrichtungsdauer unserer Produkte
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  WebServer     --> 60 Sekunden
+  KVM           --> 60 Sekunden
+  CloudServer	  --> max. 60 Sekunden
+  Web-Reseller 	--> manuelle Freischaltung
+  Dedicated     --> ca. 1 Werktag
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
