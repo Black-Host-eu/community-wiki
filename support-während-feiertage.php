58cca4153bc6f83bd6733697d49fa2651c003082
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-12-15 16:50:33 +0100

    [+] Support während den Feiertagen

---
+++ support-während-feiertage.php
@@ -0,0 +1,42 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">								
+  <a href="#der-support-über-die-feiertage" data-kt-scroll-toggle=""></a>				
+ Der Support über die Feiertage
+</h1>
+<!-- Überschrift -->	
+
+<!-- -->							
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+	
+Während den Feiertagen ist der Support nur eingeschränkt oder gar nicht erreichbar. Der Grund ist einfach: Unsere Teamler arbeiten freiwillig und werden nicht bezahlt
+  , weshalb wir genug Freizeit schaffen wollen.
+									
+</div>
+<!-- Text zur Überschrift -->
+
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->							
+<div class="pb-10">
+										
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#2021-2022" data-kt-scroll-toggle=""></a>
+    2021 - 2022
+  </h1>
+										
+  <div class="py-5">
+									
+23.12.2021 - 01.01.2022
+    <br>
+    In diesem Zeitraum sind einige Abteilungen komplett geschlossen und es ist mit verlängerten Wartezeiten zu rechnen.
+    
+  </div>
+									
+</div>
+<!-- Normaler Absatz -->
