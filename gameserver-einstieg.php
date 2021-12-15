6d94a3963bf64442b0b624f28eb9ff441a94b0e9
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 22:25:07 +0100

    [+] GameServer EInstieg

---
+++ gameserver-einstieg.php
@@ -0,0 +1,28 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#gameserver-einstieg" data-kt-scroll-toggle=""></a>
+  Wie kann ich mich ins GameServer Panel einloggen / Panel-Erklärung
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Herzlichen Glückwunsch zu deinem GameServer!
+  Hier eine Anleitung zur Verwaltung:
+  Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <img src="https://prnt.sc/235xykg" alt="">
+  Nachdem du diese offen hast, empfehlen wir, die folgende Seite in einem neuen Tab zu öffnen <a href="https://game.host-control.eu/">https://game.host-control.eu/</a>
+  Danach müsstest du folgenden Screen sehen <img src="https://prnt.sc/235ya1b" alt="">
+  Deine IP siehst du hier: <img src="https://prnt.sc/235yfro" alt="">
+  Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <img src="https://prnt.sc/235ynbf" alt="">
+
+	Weitere Fragen beantwortet dir unser <a href="https://cp.black-host.eu/tickets">Support</a> gerne.								
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
