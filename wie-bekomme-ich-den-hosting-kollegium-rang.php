08432f24120c8e2a7e9e084ed7de96f1e6a6275a
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 21:31:40 +0100

    [+] HostingKollegium Rang bekommen

---
+++ wie-bekomme-ich-den-hosting-kollegium-rang.php
@@ -0,0 +1,17 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#wie-bekomme-ich-den-hosting-kollegium-rang" data-kt-scroll-toggle=""></a>
+  Wie kann ich den Hosting-Kollegium Rang bekommen?
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Um den Rang "Hosting Kollegium" zu erhalten, muss vorab ein Ticket erstellt werden. </br> In diesem Ticket wirst du nach Informationen über dein Hosting gefragt und solltest diese beantworten. Daraufhin wird dir der Rang gegeben.
+
+</div>
