bc96957c42030f15ea90adb28016e055375a3616
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 21:56:18 +0100

    Plesk Website erstellen

---
+++ wie-erstelle-ich-eine-website.php
@@ -0,0 +1,47 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#website-erstellung-plesk" data-kt-scroll-toggle=""></a>
+  Wie erstelle ich eine WebSite in Plesk?
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Um eine Domain in deinem WebServer zu hinterlegen, befolge folgende Schritte:
+  1. Logge dich auf <a href="https://vweb01.host-control.eu">hier klicken</a>	mit den aus dem Panel erhaltenen Zugangsdaten ein.
+  2. Danach müsstest du dieses Fenster sehen: <img src="https://prnt.sc/235uipk" alt="ScreenPlesk">
+  3. Hier klickst du auf "Domain hinzufügen": <img src="https://prnt.sc/235umsx" alt="ScreenDomain">
+  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du hier <a href="HIER LINK ZU DNS VERWALTUNG"></a> <img src="https://prnt.sc/235utsd" alt="">
+  4.1 Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst: <img src="https://prnt.sc/235v4av" alt="">
+  5. Nach dem eingeben, klickst du auf Domain hinzufügen <img src="https://prnt.sc/235v9sd" alt="">
+
+  Et voila :) Deine Domain wurde hinzugefügt. Du brauchst noch weitere Hilfe?
+  Hier findest du einen <a href="https://cp.black-host.eu/tickets">Link zum Support</a>.		
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->
+<div class="pb-10">
+
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#eine-überschrift" data-kt-scroll-toggle=""></a>
+    Eine Überschrift
+  </h1>
+
+  <div class="py-5">
+
+    Dein Text
+
+  </div>
+
+</div>
+<!-- Normaler Absatz -->
