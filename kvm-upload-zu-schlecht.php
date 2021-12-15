9254e6723c04f30c42271c0ead64599c5340c0fb
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-15 18:18:03 +0100

    [+] KVM Upload

---
+++ kvm-upload-zu-schlecht.php
@@ -0,0 +1,39 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">								
+  <a href="#kvm-upload-zu-schlecht" data-kt-scroll-toggle=""></a>				
+  KVM Upload zu schlecht?
+</h1>
+<!-- Überschrift -->	
+
+<!-- -->							
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+	
+  Sollte der Upload auf deinem KVM zu schlecht sein, bitten wir dich folgende Schritte zu befolgen.					
+									
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->							
+<div class="pb-10">
+										
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#netzwerkkarte" data-kt-scroll-toggle=""></a>
+    Netzwerkkarte
+  </h1>
+										
+  <div class="py-5">
+									
+    Unter "Einstellungen" -> "VPS Konfigurieren" kann dir virtuelle Netzwerkkarte angepasst werden. Die 10 G Netzwerkkarte ist "Virtio", diese ist aber nicht standartgemäß ausgewählt. 
+    Sollte deine Verbindung immernoch nicht ausreichen, bitten wir dich dich im Support zu melden.
+							
+  </div>
+									
+</div>
+<!-- Normaler Absatz -->
