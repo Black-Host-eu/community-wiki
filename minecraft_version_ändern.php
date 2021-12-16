9275481a3510fbbc4a2d63881b193138f3179304
Scorfy2006 <80775763+Scorfy2006@users.noreply.github.com>
2021-12-16 10:55:25 +0100

    Für Fornite

---
+++ minecraft_version_ändern.php
@@ -0,0 +1,30 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">								
+  <a href="#deine-überschrift" data-kt-scroll-toggle=""></a>				
+  Wie kann ich die Minecraft Version ändern?
+</h1>
+<!-- Überschrift -->	
+
+<!-- -->							
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+    <h5>Dies ist ganz Einfach :)<h5>
+    <br><p>Sie müssen nur diese folgenden Schritte Durchführen.</p>
+    <br><p>Schritt Eins ist, Sie müssen in dem Header bereich auf der Auswahl: "Configuration" Klicken</p>
+    <br><p>Dann haben sie 3 Neue Asuwahl möglich keiten, sie klicken jetzt auf: "Advanced".</p>
+    <br><p>Dann folgt diese Ansicht:</p><img src="https://cdn.discordapp.com/attachments/868453690120486972/920976821477064754/unknown.png">
+    <br><p>Und dann ist schon fast alles gemacht, sie müssen nur noch ihr Version auswählen und viel Spaß beim Minecraft Spielen</p>
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->							
+
+									
+</div>
+<!-- Normaler Absatz -->
