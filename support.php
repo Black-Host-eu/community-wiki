a67fce07104d57d543b161f8435c70128529e5b5
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-16 18:13:01 +0100

    [+] Supportmöglichkeiten

---
+++ support.php
@@ -0,0 +1,36 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#support" data-kt-scroll-toggle=""></a>
+  Supportmöglichkeiten
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Bei uns gibt es viele Möglichkeiten, Support zu bekommen. Hier stellen wir dir einige vor:
+
+  <a href="https://discord.gg/hC8RygSGS4">Discord Support</a>	<br>
+  <a href="https://cp.black-host.eu/tickets">Web-Tickets</a> <br>
+  <a href="https://cp.black-host.eu/wiki">Wiki</a> <br>
+  <a href="mailto:partner@black-host.eu">Partnerschaftsmanagment</a> <br>
+  <a href="mailto:abuse@black-host.eu">Abuse-Meldungen</a> <br>
+  <a href="mailto:savety@black-host.eu">Sicherheit</a> <br>
+  <a href="mailto:rechtsabteilung@black-host.eu">Rechtsabteilung</a> <br>
+  <a href="mailto:buchhaltung@black-host.eu">Buchhaltung</a> <br>
+  <a href="mailto:care@black-host.eu">Wünsche / FeedBack</a>
+  <a href="mailto:info@black-host.eu">Managment</a>
+  <a href="mailto:netzwerk@black-host.eu">Netzwerk</a>
+  <a href="mailto:order@black-host.eu">Bestellungen</a>
+  <a href="mailto:verkauf@black-host.eu">Verkauf</a>
+
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
