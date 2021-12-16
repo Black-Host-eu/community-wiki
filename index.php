38b4913236d974228efb3a9ef4aa11d6df326f5b
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-12-16 09:34:20 +0100

    [+] Startseite

---
+++ index.php
@@ -0,0 +1,60 @@
+									<!--begin::Section-->
+									<div class="px-md-10 pt-md-10 pb-md-5">
+										<!--begin::Block-->
+										<div class="text-center mb-20">
+											<h1 class="fs-2tx fw-bolder mb-8">Du brauchst Hilfe? Unsere
+											<span class="d-inline-block position-relative ms-2">
+												<span class="d-inline-block mb-2">Community</span>
+												<span class="d-inline-block position-absolute h-8px bottom-0 end-0 start-0 bg-primary translate rounded"></span>
+											</span> hilft
+										</h1>
+											<div class="fw-bold fs-2 text-gray-500 mb-10">Unsere
+											<span class="fw-bolder text-gray-900">Dokumentation</span> wird von unserer <span class="fw-bolder text-gray-900">Community</span> und unseren
+											<span class="fw-bolder text-gray-900">Mitarbeitern</span> geschrieben.</div>
+										</div>
+										<!--end::Block-->
+										<!--begin::Row-->
+										<div class="row g-0">
+											<!--begin::Col-->
+											<div class="col-md-6 mb-10">
+												<div class="bg-light rounded-3 p-10 mx-md-5 h-md-100">
+													<h1 class="mb-5">KVM-Server</h1>
+													<div class="fs-4 text-gray-600 py-3">
+														Der Upload ist zu schelcht? Schau dir diesen Beitrag an, vielleicht wird dein Problem hier behoben.
+													</div>
+													<a href="<?= $url ?>kvm-upload-zu-schlecht" class="btn btn-lg btn-flex btn-link btn-color-info">Anschauen
+													<!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+													<span class="svg-icon ms-2 svg-icon-3">
+														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+															<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="black"></rect>
+															<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black"></path>
+														</svg>
+													</span>
+													<!--end::Svg Icon--></a>
+												</div>
+											</div>
+
+											<div class="col-md-6 mb-10">
+												<div class="bg-light rounded-3 p-10 mx-md-5 h-md-100">
+													<h1 class="mb-5">Einrichtungsdauer</h1>
+													<div class="fs-4 text-gray-600 py-3">
+														Hier findest du eine Liste, der Einrichtungsdauer, von unseren Produkten.
+													</div>
+													<a href="<?= $url ?>einrichtungsdauer" class="btn btn-lg btn-flex btn-link btn-color-success">Anschauen 
+													<!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+													<span class="svg-icon ms-2 svg-icon-3">
+														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+															<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="black"></rect>
+															<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black"></path>
+														</svg>
+													</span>
+													<!--end::Svg Icon--></a>
+												</div>
+											</div>
+
+
+											<!--end::Col-->
+										</div>
+										<!--end::Row-->
+									</div>
+									<!--end::Section-->
