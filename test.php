efc1a2170fb51cf5b1edf3945bafad4bdb4329ff
Simba B <75791938+SimbaAddons@users.noreply.github.com>
2021-12-16 09:27:14 +0100

    Create test.php

---
+++ test.php
@@ -0,0 +1,40 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">								
+  <a href="#wie-werden-daten-geschützt" data-kt-scroll-toggle=""></a>				
+ Wie werden Daten geschützt?
+</h1>
+<!-- Überschrift -->	
+
+<!-- -->							
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+	
+Vorab sind alle Administrationzugriffe über eine 2FA geschützt, somit haben es einige Angreifer schon mal sehr schwer. Unsere Cloudserver sind mit Nextcloud ausgestattet, diese Cloud-Software verschlüsselt deine Daten so, dass sogar wir keinen Zugriff darauf haben.			
+									
+</div>
+<!-- Text zur Überschrift -->
+
+</div>
+
+<!-- -->
+
+<!-- Normaler Absatz -->							
+<div class="pb-10">
+										
+  <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+    <a href="#die-backup-sicherheit" data-kt-scroll-toggle=""></a>
+    Die Backup Sicherheit
+  </h1>
+										
+  <div class="py-5">
+									
+Wir haben Tier7 Backups, diese werden auf 3 verschiedenen extern gehosteten Standorte gespeichert. Bei der Auswahl der Rechenzentren, haben wir vorallem auf die Sicherheit bezüglich des Brandschutes und eingen anderen Faktoren geachtet. Die Daten werden dort verschlüsselt für 7 Tagen aufbewahrt und anschließend gelöscht. Die Löschung dient zum Sparen von Speicher.
+							
+  </div>
+									
+</div>
+
+<!-- Normaler Absatz -->
