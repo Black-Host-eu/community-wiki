334f66deed38ada6c47d7f6423e97a06bb6dc02a
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-16 09:59:40 +0100

    [!] Fixxes

--- gameserver-einstieg.php
+++ gameserver-einstieg.php
@@ -14,11 +14,11 @@
 
   Herzlichen Glückwunsch zu deinem GameServer!
   Hier eine Anleitung zur Verwaltung:
-  Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <img src="https://prnt.sc/235xykg" alt="">
+  Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <img src="https://prnt.sc/235xykg.png" alt="">
   Nachdem du diese offen hast, empfehlen wir, die folgende Seite in einem neuen Tab zu öffnen <a href="https://game.host-control.eu/">https://game.host-control.eu/</a>
-  Danach müsstest du folgenden Screen sehen <img src="https://prnt.sc/235ya1b" alt="">
-  Deine IP siehst du hier: <img src="https://prnt.sc/235yfro" alt="">
-  Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <img src="https://prnt.sc/235ynbf" alt="">
+  Danach müsstest du folgenden Screen sehen <img src="https://prnt.sc/235ya1b.png" alt="">
+  Deine IP siehst du hier: <img src="https://prnt.sc/235yfro.png" alt="">
+  Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <img src="https://prnt.sc/235ynbf.png" alt="">
 
 	Weitere Fragen beantwortet dir unser <a href="https://cp.black-host.eu/tickets">Support</a> gerne.								
 </div>
