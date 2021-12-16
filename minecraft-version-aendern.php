f6124f35e08772c41d828e726a2d186bfc8f1a51
Scorfy2006 <80775763+Scorfy2006@users.noreply.github.com>
2021-12-16 11:08:28 +0100

    Update minecraft-version-aendern.php

--- minecraft-version-aendern.php
+++ minecraft-version-aendern.php
@@ -16,6 +16,7 @@
     <br><p>Schritt Eins ist, Sie müssen in dem Header bereich auf der Auswahl: "Configuration" Klicken</p>
     <br><p>Dann haben sie 3 Neue Auswahl möglich keiten, sie klicken jetzt auf: "Advanced".</p>
     <br><p>Dann folgt diese Ansicht:</p><img src="https://data.black-host.eu/share/version-%C3%A4ndern.png">
+    <br>
     <br><p>Und dann ist schon fast alles gemacht, sie müssen nur noch ihr Version auswählen und viel Spaß beim Minecraft Spielen</p>
 </div>
 <!-- Text zur Überschrift -->
