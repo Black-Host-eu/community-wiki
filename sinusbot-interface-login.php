a55204ce4f2336c4ed8917f2da6c4e20c9581f45
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-16 23:09:33 +0100

    [+] Sinusbot Interface login

---
+++ sinusbot-interface-login.php
@@ -0,0 +1,25 @@
+<div class="pb-10">
+
+    <!-- Überschrift -->
+    <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+        <a href="#sinusbot-interface-login" data-kt-scroll-toggle=""></a>
+        Wie logge ich mich im Sinusbot Interface ein?
+    </h1>
+    <!-- Überschrift -->
+
+    <!-- -->
+
+    <!-- Text zur Überschrift -->
+    <div class="py-5">
+
+        <p>Schritt 1: Zuerst musst du dich bei uns im Gameserver Interface (game.host-control.eu) anmelden, um dir die IP Adresse und den Port von deinem Sinusbot zu kopieren.</p>
+        <br><p>Schritt 2: Dort musst du dir dann die IP Adresse und den Port kopieren. Da solltest du nun einen Server finden, der Sinusbot Instanz heißt. Die IP Adresse und den Port kannst du direkt unten in den Information finden. (Siehe Bild)</p><img src="https://cdn.jonas-budde.de/screenshots/Oyv95DqigRkn7W0iYLILhQzMsqWhFGlS.png">
+        <br><p>Schritt 3: Die IP Adresse und den Port musst du nun mit deinem Webbowser aufrufen. Dann landest du im Interface von Sinusbot. Die Anmeldedaten dafür findest du bei uns im Webinterface unter Sinusbot-Zugang. (Siehe Bild)</p><img src="https://cdn.jonas-budde.de/screenshots/gIDGMNeUXZNkkEjVGVMUWaqygfNbs397.png">
+        <br>
+        <p>Nun bist du im Sinusbot Interface und kannst dein Sinusbot verwalten.</p>
+
+    </div>
+    <!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
