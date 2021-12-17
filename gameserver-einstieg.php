7e301445e8e72df95f04b1808d8488d50a20ab87
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-17 15:26:56 +0100

    [!] LineBreaker eingesetzt

--- gameserver-einstieg.php
+++ gameserver-einstieg.php
@@ -13,12 +13,12 @@
 <div class="py-5">
 
   Herzlichen Glückwunsch zu deinem GameServer!
-  Hier eine Anleitung zur Verwaltung:
-  Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180253.png" alt="">
-  Nachdem du diese offen hast, empfehlen wir, die folgende Seite in einem neuen Tab zu öffnen <a href="https://game.host-control.eu/">https://game.host-control.eu/</a>
-  Danach müsstest du folgenden Screen sehen <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180310.png" alt="">
-  Deine IP siehst du hier: <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180328.png" alt="">
-  Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180346.png" alt="">
+  Hier eine Anleitung zur Verwaltung: <br>
+  Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180253.png" alt=""><br>
+  Nachdem du diese offen hast, empfehlen wir, die folgende Seite in einem neuen Tab zu öffnen <a href="https://game.host-control.eu/">https://game.host-control.eu/</a> <br>
+  Danach müsstest du folgenden Screen sehen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180310.png" alt="">
+  Deine IP siehst du hier: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180328.png" alt=""><br>
+  Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180346.png" alt=""><br>
 
 	Weitere Fragen beantwortet dir unser <a href="https://cp.black-host.eu/tickets">Support</a> gerne.								
 </div>
