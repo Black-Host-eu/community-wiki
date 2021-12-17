ad83f2d1b1a434ea212bf8fc7d6de10aa167d950
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-17 15:31:14 +0100

    [!] Neue Linebreaker

--- gameserver-einstieg.php
+++ gameserver-einstieg.php
@@ -16,7 +16,7 @@
   Hier eine Anleitung zur Verwaltung: <br>
   Nach deiner Bestellung siehst du deine Login-Daten indem du auf Verwalten klickst: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180253.png" alt=""><br>
   Nachdem du diese offen hast, empfehlen wir, die folgende Seite in einem neuen Tab zu öffnen <a href="https://game.host-control.eu/">https://game.host-control.eu/</a> <br>
-  Danach müsstest du folgenden Screen sehen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180310.png" alt="">
+  Danach müsstest du folgenden Screen sehen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180310.png" alt=""> <br>
   Deine IP siehst du hier: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180328.png" alt=""><br>
   Über diese Buttons kannst du deinen Server Starten / Stoppen / Hardstoppen: <br><img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_180346.png" alt=""><br>
 
