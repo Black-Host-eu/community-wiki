c3be006e69956ac7c08a81629d773bd1b22c34ef
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-17 15:47:54 +0100

    [+] KVM Root PW ändern

---
+++ kvm-passwd.php
@@ -0,0 +1,41 @@
+<div class="pb-10">
+
+<!-- Überschrift -->
+<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+  <a href="#passwd" data-kt-scroll-toggle=""></a>
+  Wie ändere ich mein KVM Passwort?
+</h1>
+<!-- Überschrift -->
+
+<!-- -->
+
+<!-- Text zur Überschrift -->
+<div class="py-5">
+
+  Um dein Passwort zu ändern führe folgende Commands via SSH durch:
+
+  <div class="highlight">
+    <!-- Start Copy Section -->
+
+  <button class="highlight-copy btn" data-bs-toggle="tooltip" title="" data-bs-original-title="Kopiere die folgenden Zeilen">
+    kopieren
+  </button>
+
+
+  <div class="highlight-code">
+    <pre class=" language-bash" tabindex="0">
+    <code class=" language-bash">
+          <!-- Code hier Start -->
+  passwd
+  # hier neues Passwort eingeben, dann Enter drücken :)
+          <!-- Code hier Ende -->
+    </code>
+    </pre>
+  </div>
+</div> <!-- Ende Copy Section -->
+
+</div>
+<!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
