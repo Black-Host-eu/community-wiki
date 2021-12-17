4cb4d50362063ccf5c480802cb7695ba2e978ce5
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-17 21:09:42 +0100

    [+] Sinusbot mit einem Teamspeak Server verbinden

---
+++ sinusbot-mit-teamspeak-verbinden.php
@@ -0,0 +1,28 @@
+<div class="pb-10">
+
+    <!-- Überschrift -->
+    <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+        <a href="#sinusbot-mit-teamspeak-verbinden" data-kt-scroll-toggle=""></a>
+        Wie verbinde ich den Sinusbot mit dem Teamspeak Server?
+    </h1>
+    <!-- Überschrift -->
+
+    <!-- -->
+
+    <!-- Text zur Überschrift -->
+    <div class="py-5">
+
+        <p>Schritt 1: Zuerst musst du dich in das Sinusbot Interface einloggen. Solltest du nicht wissen, wie das geht, schau in unseren Wiki Artikel.</p>
+        <br><p>Schritt 2: Dann musst du oben in der Navbar unter Settings und Instance Settings gehen. Dort kannst du dann alle wichtigen Information zu deinem Teamspeak Server hinterlegen.</p>
+        <br><p>Schritt 3: Du musst auf jeden Fall einen Nicknamen, den Hostname/IP und den Port angeben. Falls dein Teamspeak Server mit einem Passwort geschützt ist, musst du dieses auch noch unter Password angeben. Der Rest ist nicht notwendig, aber kann noch angegeben werden wie z. B. Default Channel das dazugehörige Channel Passwort. Nun kannst du den Bot starten, wenn du nicht weißt, wie das geht schau in unser Wiki.</p>
+        <br>
+        <h5>Falls du noch eine zweite Teamspeak Instanz erstellen möchtest, musst du Folgendes machen:</h5>
+        <p>Schritt 1: In der Navbar oben wieder auf Settings und dann unter Instances</p>
+        <p>Schritt 2: Nun musst du auf Add Instance gehen. Dies findest du oben Rechts. Dort musst du dann Teamspeak 3 auswählen und einen belieben Nicknamen geben und dann auf Create.</p>
+        <p>Schon ist eine weiter Instanz erstellt. Nun kannst du wieder mit den vorherigen Schritten anfangen, um die Instanz einzurichten.</p>
+
+    </div>
+    <!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
