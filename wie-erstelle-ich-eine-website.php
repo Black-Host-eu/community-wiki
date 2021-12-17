aecc2e975f0e041cea0640f2687d78d267701920
Whity07 <82459939+Whity07@users.noreply.github.com>
2021-12-17 15:38:23 +0100

    [!] Bilder geändert

--- wie-erstelle-ich-eine-website.php
+++ wie-erstelle-ich-eine-website.php
@@ -14,13 +14,13 @@
 
   Um eine Domain in deinem WebServer zu hinterlegen, befolge folgende Schritte:
   1. Logge dich auf <a href="https://vweb01.host-control.eu">hier klicken</a>	mit den aus dem Panel erhaltenen Zugangsdaten ein.
-  2. Danach müsstest du dieses Fenster sehen: <img widht="50" src="https://data.black-host.eu/share/Bild_2021-12-16_162253.png" alt="ScreenPlesk">
-  3. Hier klickst du auf "Domain hinzufügen": <img src="https://data.black-host.eu/share/Bild_2021-12-16_162404.png" alt="ScreenDomain">
-  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du <a href="dns-einträge.php">hier </a>. Nachdem du das gemacht hast, kannst du die Daten hier eintragen <img src="https://data.black-host.eu/share/Bild_2021-12-16_162432.png" alt="">
-  4.1 Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst: <img src="https://data.black-host.eu/share/Bild_2021-12-16_162504.png" alt="">
-  5. Nach dem eingeben, klickst du auf Domain hinzufügen <img src="https://data.black-host.eu/share/Bild_2021-12-16_163806.png" alt="">
+  2. Danach müsstest du dieses Fenster sehen: <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162253.png" alt="ScreenPlesk"> <br>
+  3. Hier klickst du auf "Domain hinzufügen": <br> <img src="https://data.black-host.eu/share/Bild_2021-12-16_162404.png" alt="ScreenDomain"> <br>
+  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du <a href="dns-einträge.php">hier </a>. Nachdem du das gemacht hast, kannst du die Daten hier eintragen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162432.png" alt=""> <br>
+  4.1 Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst:<br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162504.png" alt=""><br>
+  5. Nach dem eingeben, klickst du auf Domain hinzufügen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_163806.png" alt=""><br>
 
-  Et voila :) Deine Domain wurde hinzugefügt. Du brauchst noch weitere Hilfe?
+  Et voila :) Deine Domain wurde hinzugefügt. Du brauchst noch weitere Hilfe? <br>
   Hier findest du einen <a href="https://cp.black-host.eu/tickets">Link zum Support</a>.		
 
 </div>
