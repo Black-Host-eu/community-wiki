1bb95f307ab0b77019dcab7e569ba46a403d4663
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-18 19:30:50 +0100

    [!] Bildergröße angepasst

--- wie-erstelle-ich-eine-website.php
+++ wie-erstelle-ich-eine-website.php
@@ -1,32 +1,24 @@
 <div class="pb-10">
 
-<!-- Überschrift -->
-<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
-  <a href="#website-erstellung-plesk" data-kt-scroll-toggle=""></a>
-  Wie erstelle ich eine WebSite in Plesk?
-</h1>
-<!-- Überschrift -->
+    <!-- Überschrift -->
+    <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+        <a href="#website-erstellung-plesk" data-kt-scroll-toggle=""></a>
+        Wie erstelle ich eine Webseite in Plesk?
+    </h1>
+    <!-- Überschrift -->
 
-<!-- -->
+    <!-- -->
 
-<!-- Text zur Überschrift -->
-<div class="py-5">
+    <!-- Text zur Überschrift -->
+    <div class="py-5">
 
-  Um eine Domain in deinem WebServer zu hinterlegen, befolge folgende Schritte: <br>
-  1. Logge dich auf <a href="https://vweb01.host-control.eu">hier klicken</a>	mit den aus dem Panel erhaltenen Zugangsdaten ein. <br>
-  2. Danach müsstest du dieses Fenster sehen: <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162253.png" alt="ScreenPlesk"> <br>
-  3. Hier klickst du auf "Domain hinzufügen": <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162404.png" alt="ScreenDomain"> <br>
-  4. Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du <a href="dns-einträge.php">hier </a>. Nachdem du das gemacht hast, kannst du die Daten hier eintragen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162432.png" alt=""> <br>
-  4.1 Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst:<br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_162504.png" alt=""><br>
-  5. Nach dem eingeben, klickst du auf Domain hinzufügen <br> <img width="100" src="https://data.black-host.eu/share/Bild_2021-12-16_163806.png" alt=""><br>
-
-  Et voila :) Deine Domain wurde hinzugefügt. Du brauchst noch weitere Hilfe? <br>
-  Hier findest du einen <a href="https://cp.black-host.eu/tickets">Link zum Support</a>.		
-
+        <h5>Um eine Domain in deinem Webspace zu hinterlegen, befolge folgende Schritte:</h5>
+        <br><p>Schritt 1: Logge dich auf <a href="https://vweb01.host-control.eu">Plesk</a>	mit den aus dem Panel erhaltenen Zugangsdaten ein.</p>
+        <br><p>Schritt 2: Hier klickst du auf "Domain hinzufügen" (Siehe Bild).</p><br><img width="1100" src="https://data.black-host.eu/share/Bild_2021-12-16_162404.png" alt="ScreenDomain"><br>
+        <br><p>Schritt 3: Hier gibst du deine Domain-Daten ein, aber zuerst musst du einige DNS-Einträge setzen. Wie das funktioniert, findest du <a href="dns-einträge.php">hier </a>. Nachdem du das gemacht hast, kannst du die Daten hier eintragen</p><br><img width="1100" src="https://data.black-host.eu/share/Bild_2021-12-16_162432.png" alt=""><br>
+        <br><p>Schritt 4:  Wenn du noch keine Domain hast, kannst du diese entweder <a href="https://cp.black-host.eu/order/domain">hier erwerben</a> oder eine Plesk-Subdomain nutzen. Diese kannst du bekommen, indem du hier klickst:</p><br><img width="1100" src="https://data.black-host.eu/share/Bild_2021-12-16_162504.png" alt=""><br>
+        <br><p>Schritt 5: Nach dem eingeben, klickst du auf Domain hinzufügen</p><br><img width="1100" src="https://data.black-host.eu/share/Bild_2021-12-16_163806.png" alt=""><br>
+    </div>
+    <!-- Text zur Überschrift -->
 </div>
-<!-- Text zur Überschrift -->
-</div>
-
 <!-- -->
-
-
