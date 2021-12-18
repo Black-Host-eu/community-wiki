4ee9a8980ef7d9c7fe9455b97b2a04bc965cf83c
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-18 19:12:45 +0100

    [!] Designanpassung

--- dns-eintraege.php
+++ dns-eintraege.php
@@ -1,28 +1,93 @@
 <div class="pb-10">
 
-<!-- Überschrift -->
-<h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
-  <a href="#dns" data-kt-scroll-toggle=""></a>
-  Welche DNS Einträge brauche ich für den WebServer?
-</h1>
-<!-- Überschrift -->
+    <!-- Überschrift -->
+    <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+        <a href="#dns" data-kt-scroll-toggle=""></a>
+        Welche DNS Einträge brauche ich für meinen Webspace?
+    </h1>
+    <!-- Überschrift -->
 
-<!-- -->
+    <!-- -->
 
-<!-- Text zur Überschrift -->
-<div class="py-5">
+    <!-- Text zur Überschrift -->
+    <div class="py-5">
 
-  Für unseren VWEB01 musst du für deine Domain folgende Einträge setzen.
- 	Da ich deine Domain nicht kenne, habe ich den Platzhalter deinedomain.de gewählt. Dies ist aber analog bei allen anderen Domains.
-  Also - du musst überall deinedomain.de durch deine Domain (bspw. black-host.eu, google.de) <br>
+        <p>Schritt 1: Damit du deine Domain auf deinem Webspace nutzen kannst, musst du die unten stehenden DNS Einträge bei deiner Domain hinterlegen. Wenn du eine Subdomain nutzen möchtest, musst du den untersten DNS Eintrag auch setzten, ansonsten kannst du diesen weglassen.</p><br>
 
-  @         --> A     --> 88.198.11.120 <br>
-  webmail   --> A     --> 88.198.11.120 <br>
-  mail      --> A     --> 88.198.11.120 <br>
-  @         --> MX(10)--> mail.deinedomain.de <br>
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Type</th>
+                <th scope="col">Name</th>
+                <th scope="col">Ziel</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>A</td>
+                <td>@</td>
+                <td>IPv4 von vweb01</td>
+            </tr>
+            <tr>
+                <td>A</td>
+                <td>www</td>
+                <td>IPv4 von vweb01</td>
+            </tr>
+            <tr>
+                <td>A</td>
+                <td>subdomain</td>
+                <td>IPv4 von vweb01</td>
+            </tr>
+            </tbody>
+        </table>
 
-</div>
-<!-- Text zur Überschrift -->
+        <p>Schritt 2: Die IPv4 von vweb01 findest du in Plesk. Dazu musst du dich anmelden und findest die direkt auf der Startseite (Siehe Bild).</p><br><img width="1100" src="https://cdn.jonas-budde.de/screenshots/9DuLThjHQLyPLqXyRI2A46k2umXUZhQQ.png"><br>
+
+    </div>
+    <!-- Text zur Überschrift -->
+
+    <div class="row g-0">
+        <!--begin::Col-->
+        <div class="col-md-6 mb-10">
+            <div class="bg-light rounded-3 p-10 mx-md-5 h-md-100">
+                <h1 class="mb-5">Domain in Plesk hinterlegen</h1>
+                <div class="fs-4 text-gray-600 py-3">
+                    Hier findest du eine Anleitung, wie du nun deine Domain in Plesk hinterlegen kannst und so deine eigene Webseite erstellst.
+                </div>
+                <a href="wie-erstelle-ich-eine-webseite" class="btn btn-lg btn-flex btn-link btn-color-info">Anschauen
+                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+                    <span class="svg-icon ms-2 svg-icon-3">
+														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+															<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="black"></rect>
+															<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black"></path>
+														</svg>
+													</span>
+                    <!--end::Svg Icon--></a>
+            </div>
+        </div>
+
+        <div class="col-md-6 mb-10">
+            <div class="bg-light rounded-3 p-10 mx-md-5 h-md-100">
+                <h1 class="mb-5">Eigene E-Mail Adressen erstellen</h1>
+                <div class="fs-4 text-gray-600 py-3">
+                    Hier findest du eine Anleitung wie, du in Plesk mit deiner eigenen Domain eine E-Mail Adresse erstellen kannst.
+                </div>
+                <a href="email-adresse-einrichten" class="btn btn-lg btn-flex btn-link btn-color-success">Anschauen
+                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr064.svg-->
+                    <span class="svg-icon ms-2 svg-icon-3">
+														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+															<rect opacity="0.5" x="18" y="13" width="13" height="2" rx="1" transform="rotate(-180 18 13)" fill="black"></rect>
+															<path d="M15.4343 12.5657L11.25 16.75C10.8358 17.1642 10.8358 17.8358 11.25 18.25C11.6642 18.6642 12.3358 18.6642 12.75 18.25L18.2929 12.7071C18.6834 12.3166 18.6834 11.6834 18.2929 11.2929L12.75 5.75C12.3358 5.33579 11.6642 5.33579 11.25 5.75C10.8358 6.16421 10.8358 6.83579 11.25 7.25L15.4343 11.4343C15.7467 11.7467 15.7467 12.2533 15.4343 12.5657Z" fill="black"></path>
+														</svg>
+													</span>
+                    <!--end::Svg Icon--></a>
+            </div>
+        </div>
+
+
+        <!--end::Col-->
+    </div>
+
 </div>
 
 <!-- -->
