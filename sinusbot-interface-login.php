c095b6abd9aee1a1d452a61dbf6c9206f3102311
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-18 04:21:58 +0100

    [!] Abstand zwischen Bildern und Text angepasst

--- sinusbot-interface-login.php
+++ sinusbot-interface-login.php
@@ -12,9 +12,9 @@
     <!-- Text zur Überschrift -->
     <div class="py-5">
 
-        <p>Schritt 1: Zuerst musst du dich bei uns im Gameserver Interface (game.host-control.eu) anmelden, um dir die IP Adresse und den Port von deinem Sinusbot zu kopieren.</p>
-        <br><p>Schritt 2: Dort musst du dir dann die IP Adresse und den Port kopieren. Da solltest du nun einen Server finden, der Sinusbot Instanz heißt. Die IP Adresse und den Port kannst du direkt unten in den Information finden. (Siehe Bild)</p><img src="https://cdn.jonas-budde.de/screenshots/Oyv95DqigRkn7W0iYLILhQzMsqWhFGlS.png">
-        <br><p>Schritt 3: Die IP Adresse und den Port musst du nun mit deinem Webbowser aufrufen. Dann landest du im Interface von Sinusbot. Die Anmeldedaten dafür findest du bei uns im Webinterface unter Sinusbot-Zugang. (Siehe Bild)</p><img src="https://cdn.jonas-budde.de/screenshots/gIDGMNeUXZNkkEjVGVMUWaqygfNbs397.png">
+        <p>Schritt 1: Zuerst musst du dich bei uns im <a href="https://game.host-control.eu/" target="_blank">Gameserver Interface</a> anmelden, um dir die IP Adresse und den Port von deinem Sinusbot zu kopieren.</p>
+        <br><p>Schritt 2: Dort musst du dir dann die IP Adresse und den Port kopieren. Da solltest du nun einen Server finden, der Sinusbot Instanz heißt. Die IP Adresse und den Port kannst du direkt unten in den Information finden. (Siehe Bild)</p><br><img src="https://cdn.jonas-budde.de/screenshots/Oyv95DqigRkn7W0iYLILhQzMsqWhFGlS.png"><br>
+        <br><p>Schritt 3: Die IP Adresse und den Port musst du nun mit deinem Webbowser aufrufen. Dann landest du im Interface von Sinusbot. Die Anmeldedaten dafür findest du bei uns im Webinterface unter Sinusbot-Zugang. (Siehe Bild)</p><br><img src="https://cdn.jonas-budde.de/screenshots/gIDGMNeUXZNkkEjVGVMUWaqygfNbs397.png"><br>
         <br>
         <p>Nun bist du im Sinusbot Interface und kannst dein Sinusbot verwalten.</p>
 
