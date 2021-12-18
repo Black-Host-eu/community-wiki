72b8db7664ae348c1da6365241c60a50b16ddf96
Atomkrieger03 <49342738+Atomkrieger03@users.noreply.github.com>
2021-12-18 04:32:00 +0100

    [+] Sinusbot starten

---
+++ sinusbot-starten.php
@@ -0,0 +1,29 @@
+<div class="pb-10">
+
+    <!-- Überschrift -->
+    <h1 class="anchor fw-bolder mb-5" data-kt-scroll-offset="85" id="">
+        <a href="#wie-starte-ich-den-sinusbot" data-kt-scroll-toggle=""></a>
+        Wie starte ich einen Sinusbot?
+    </h1>
+    <!-- Überschrift -->
+
+    <!-- -->
+
+    <!-- Text zur Überschrift -->
+    <div class="py-5">
+
+        <h5>Möglichkeit 1</h5>
+        <br><p>Schritt 1: Zuerst musst du dich in das Sinusbot Interface einloggen. Solltest du nicht wissen, wie das geht, schau in unseren Wiki Artikel.</p>
+        <br><p>Schritt 2: Du musst in der Navbar unter Settings und dann Instances gehen. Dort werden alle deine Instanzen gelistet.</p>
+        <br><p>Schritt 3: Dann musst du bei der gewünschten Instanz auf den Start/Stop Button klicken (Siehe Bild) damit startest oder stopst den Bot.</p><br><img width="1100" src="https://cdn.jonas-budde.de/screenshots/6vh8KiSsYdyTo8Apx4D6KtdhXPpmG4Lg.png"><br>
+        <br>
+        <h5>Möglichkeit 2</h5>
+        <br><p>Schritt 1: Zuerst musst du dich im Sinusbot Interface einloggen. Zuerst musst du dich in das Sinusbot Interface einloggen. Solltest du nicht wissen, wie das geht, schau in unseren Wiki Artikel.</p>
+        <br><p>Schritt 2: Du hast oben in der Navbar eine Auswahl zwischen den Instanzen (Ist nur da, wenn auch eine zweite Instanz erstellt ist). Dort kannst du die gewünschte Instanz auswählen, wenn nur eine Instanz existiert, brauchst du keine Auswählen.</p><br><img src="https://cdn.jonas-budde.de/screenshots/4Rc3f8Dgu5vt9EHMMb4NY923tZ0o49Xj.png"><br>
+        <br><p>Schritt 3: Dann musst du nur noch in der Navbar auf den Start/Stop Button klicken (Siehe Bild) damit startest oder stoppst den Bot.</p><br><img src="https://cdn.jonas-budde.de/screenshots/YNtKPkuAEFlKnziKW8gCjbDSa4b2yCrs.png">
+
+    </div>
+    <!-- Text zur Überschrift -->
+</div>
+
+<!-- -->
